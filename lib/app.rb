136c4f790f42f4b3b62400b64d95301595138556
Arda Cicek <ardacck@gmail.com>
2016-02-01 13:04:47 +0200

    unnested migration

--- lib/app.rb
+++ lib/app.rb
@@ -14,77 +14,6 @@ end
 
 def create_report
 
-	#Method that creates ascii art headers
-	def print_ascii_art_header(header_name)
-		header_ascii = Artii::Base.new
-		puts header_ascii.asciify(header_name)
-	end
-
-	#Method prints current date
-	def print_date
-		current_time = DateTime.now
-		date_today = current_time.strftime "%d/%m/%Y"
-		puts "Today's date is: " + date_today 
-	end
-
-	setup_files
-
-	#Method includes report products
-	def report_products
-	$toys_data["items"].each { |toy| 
-	    puts toy["title"] #prints name of the toy
-	    puts "**************************************"   #Saperator
-	    puts "The retail price : " + toy["full-price"].to_s + " USD" #prints retail price of the toy
-	    puts "Total Purchases  : #{toy["purchases"].length.to_i}" #prints total number of purchases
-	    
-	    total_amount = 0    # Calculates and prints the total amount of sales
-	    toy["purchases"].each { |purchase|
-	    total_amount += purchase["price"].to_f
-	    }
-	    puts "Total Sales      : #{total_amount} USD"
-
-	    total_amount = 0                            # Calculate and print the average price the toy sold for  !!!!!!!!!!!!!!!!!!!!wrong               
-	    toy["purchases"].each { |purchase|
-	      total_amount += purchase["price"].to_f
-	    }
-	    puts "Average Price    : #{total_amount/toy["purchases"].length} USD" 
-
-	    puts "Average Discount : #{toy["full-price"].to_f.round(2) - total_amount.round(2)/toy["purchases"].length.round(2)} USD"
-	    
-
-	    puts "                                      " #Empty Line
-	    }
-	end
-
-	#Method includes report brands
-	def report_brands
-	unique_brands = $toys_data["items"].map { |item| item["brand"] }.uniq
-	  unique_brands.each_with_index { |brand, index|
-	    puts " "
-	    puts brand #prints the name of the brand
-	    puts "**************************************"   #Saperator
-	    brand_toys = $toys_data["items"].select { |item| item["brand"] == brand }
-
-	    total_stock_brand = 0
-	    full_actual_price = 0
-	    brand_purchases = 0
-	    brand_sales = 0
-
-	    brand_toys.each { |toy| total_stock_brand += toy["stock"].to_i } #counts stock for each brand
-	    brand_toys.each { |item| brand_purchases += item["purchases"].length.to_i }
-	    brand_toys.each { |item| 
-	    item["purchases"].each { |el| brand_sales+= el["price"].to_f
-	        }
-	      }
-	    brand_toys.each { |item| full_actual_price += (item["full-price"].to_f) }
-	    puts "Total Stock         : #{total_stock_brand}"
-	    puts "Total Revenue       : #{brand_sales.round(2)} USD"
-	    average_brand_price = (brand_sales / brand_purchases)
-	    average_brand_disc = (1 - brand_sales / (full_actual_price*2).to_f)
-	    puts "Average Brand Price : #{average_brand_price.round(2)} USD"
-	}
-	end
-
 	# Print "sales seport" in ascii art
 	print_ascii_art_header("sales report")
 
@@ -104,11 +33,80 @@ def create_report
 	report_brands
 
 end
+#Method that creates ascii art headers
+def print_ascii_art_header(header_name)
+    header_ascii = Artii::Base.new
+    puts header_ascii.asciify(header_name)
+end
+
+#Method prints current date
+def print_date
+    current_time = DateTime.now
+    date_today = current_time.strftime "%d/%m/%Y"
+    puts "Today's date is: " + date_today
+end
+
+setup_files
+
+#Method includes report products
+def report_products
+    $toys_data["items"].each { |toy|
+        puts toy["title"] #prints name of the toy
+        puts "**************************************"   #Saperator
+        puts "The retail price : " + toy["full-price"].to_s + " USD" #prints retail price of the toy
+        puts "Total Purchases  : #{toy["purchases"].length.to_i}" #prints total number of purchases
+        
+        total_amount = 0    # Calculates and prints the total amount of sales
+        toy["purchases"].each { |purchase|
+            total_amount += purchase["price"].to_f
+        }
+        puts "Total Sales      : #{total_amount} USD"
+        
+        total_amount = 0                            # Calculate and print the average price the toy sold for  !!!!!!!!!!!!!!!!!!!!wrong
+        toy["purchases"].each { |purchase|
+            total_amount += purchase["price"].to_f
+        }
+        puts "Average Price    : #{total_amount/toy["purchases"].length} USD"
+        
+        puts "Average Discount : #{toy["full-price"].to_f.round(2) - total_amount.round(2)/toy["purchases"].length.round(2)} USD"
+        
+        
+        puts "                                      " #Empty Line
+    }
+end
+
+#Method includes report brands
+def report_brands
+    unique_brands = $toys_data["items"].map { |item| item["brand"] }.uniq
+    unique_brands.each_with_index { |brand, index|
+        puts " "
+        puts brand #prints the name of the brand
+        puts "**************************************"   #Saperator
+        brand_toys = $toys_data["items"].select { |item| item["brand"] == brand }
+        
+        total_stock_brand = 0
+        full_actual_price = 0
+        brand_purchases = 0
+        brand_sales = 0
+        
+        brand_toys.each { |toy| total_stock_brand += toy["stock"].to_i } #counts stock for each brand
+        brand_toys.each { |item| brand_purchases += item["purchases"].length.to_i }
+        brand_toys.each { |item|
+            item["purchases"].each { |el| brand_sales+= el["price"].to_f
+            }
+        }
+        brand_toys.each { |item| full_actual_price += (item["full-price"].to_f) }
+        puts "Total Stock         : #{total_stock_brand}"
+        puts "Total Revenue       : #{brand_sales.round(2)} USD"
+        average_brand_price = (brand_sales / brand_purchases)
+        average_brand_disc = (1 - brand_sales / (full_actual_price*2).to_f)
+        puts "Average Brand Price : #{average_brand_price.round(2)} USD"
+    }
+end
 
 File.open("report.txt", "w+") do |line|
 		line.puts create_report
 		
-
 end 
 
 
